b011fc65ce51633e6ebe69fbea6a7614e4c5873c
PedroLavin <63043948+PedroLavin@users.noreply.github.com>
2020-07-15 18:22:55 +0200

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,7 +20,11 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+from skelearn import svm
+clf=svmSVC()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+SVC()
 
 #########################################################
 ### your code goes here ###
